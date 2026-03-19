de703139c651a34c71d67ea8d407515d9d8fc58b
seanhe43 <112118043+seanhe43@users.noreply.github.com>
2026-03-19 01:46:44 -0400

    added css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import AlbumCard from "../components/AlbumCard";
+import "../css/Home.css"
 import { useState } from "react";
 
 function Home() {
@@ -8,6 +9,13 @@ function Home() {
     { id: 1, title: "AOTY", release_date: "2025" },
     { id: 2, title: "Bops", release_date: "2023" },
     { id: 3, title: "Pop", release_date: "2020" },
+    { id: 4, title: "Time Travel", release_date: "2030" },
+    { id: 5, title: "Jams", release_date: "2028" },
+    { id: 6, title: "AOTY", release_date: "2025" },
+    { id: 7, title: "Bops", release_date: "2023" },
+    { id: 8, title: "Pop", release_date: "2020" },
+    { id: 9, title: "Time Travel", release_date: "2030" },
+    { id: 10, title: "Jams", release_date: "2028" },
   ];
 
   const handleSearch = (e) => {
